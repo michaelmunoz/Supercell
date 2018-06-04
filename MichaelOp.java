f3d98757eb90f7feaddb2dd161c463ba54863536
Michael Munoz <michael.munoz11@yahoo.com>
2018-06-03 22:05:46 -0500

    Improved Commenting

--- MichaelOp.java
+++ MichaelOp.java
@@ -1,4 +1,17 @@
 package org.firstinspires.ftc.teamcode.vv7797.opmode;
+/**
+ * TeleOp written by FTC Team 7797 for the 2017-2018 Relic Recovery Game
+ *
+ * This code was designed and written for the most convenient and enjoyable driver experience.
+ *
+ * The TeleOp allows drivers to effortlessly move in any direction with the help of polar
+ * mathematics, turn with ease thanks to "dual zone" analog stick calculations, toggle intake
+ * motors and ramp positions with the push of a button, extend and retract linear slide, and
+ * control servos and other ammenities with ease...
+ *
+ * Michael Munoz, 2017/18 7797 Software Co-Lead
+ * michaelmunoz1101@gmail.com
+ */
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -14,30 +27,41 @@ import org.firstinspires.ftc.teamcode.vv7797.opmode.util.exception.AbortExceptio
 
 import java.util.ArrayList;
 
-
-//import org.firstinspires.ftc.teamcode.vv7797.opmode.control.VictorianGamepad;
-
 @TeleOp(name = "Supercell Michael")
-// @Disabled
+// Class header
 public class MichaelOp extends OpMode {
+    // Variable Declarations
+    // Declare Hardware (Motors and servos to be used during TeleOp)
     private DcMotorEx dtFrontLeft, dtFrontRight, dtBackLeft, dtBackRight;
     private DcMotorEx drvIntakeLeft, drvIntakeRight, drvLinearSlide, drvVacuum;
     private Servo srvRamp, srvPhone, srvRelicArm, srvRelicClaw, srvArmSwivel, srvArmShoulder, srvLatch;
-    private ElapsedTime runtime = new ElapsedTime();
-    private boolean intake, intakeReverse, ramp, limiter, arm, claw, latch;
-    private boolean cTIntake, cTIntakeReverse, cTRamp, cTLimiter, cTArm, cTClaw, cTCorrector, cTLatch;
-    private final double INTAKE_CAP = 0.25, DRIVE_CAP = 0.6;
-    //private final double DRIVE_TO_STRAFE_RATIO = 1.447375;
+    // Declare Servo Positions And Motor Speed Limiters
+    private final double INTAKE_CAP = 0.25;
+    private final double DRIVE_CAP = 0.6;
+    private final double EPSILON = Math.sqrt(2);
     private final double RELIC_ARM_POSITION_DOWN = 0;
     private final double RELIC_ARM_POSITION_UP = 1;
     private final double RELIC_CLAW_POSITION_CLOSED = 1;
     private final double RELIC_CLAW_POSITION_OPEN = 0.33;
-    private double LFP, LRP, RFP, RRP, driveAngle, intakePower, INTAKE_MOD = INTAKE_CAP, DRIVE_MOD = DRIVE_CAP;
+    // Declare Boolean Variables
+    private boolean intake, intakeReverse, ramp, limiter, arm, claw, latch;
+    private boolean cTIntake, cTIntakeReverse, cTRamp, cTLimiter, cTArm, cTClaw, cTCorrector, cTLatch;
+    // Declare Motor Power Variables
+    private double LFP, LRP, RFP, RRP, driveAngle, intakePower;
+    private double INTAKE_MOD = INTAKE_CAP, DRIVE_MOD = DRIVE_CAP;
+    // Declare Data Structures
     private ArrayList<ConcurrentInstruction> schedule = new ArrayList<>();
     private DcMotorEx[] drivetrain;
+    private ElapsedTime runtime = new ElapsedTime();
 
+
+    /**
+     * Initialize all hardware on the robot and any variables used to control the robot
+     * Executes during TeleOp initialization
+     */
     @Override
     public void init() {
+        // Instantiate motors
         dtFrontLeft = (DcMotorEx)hardwareMap.dcMotor.get("drvFrontLeft");
         dtFrontRight = (DcMotorEx)hardwareMap.dcMotor.get("drvFrontRight");
         dtBackLeft = (DcMotorEx)hardwareMap.dcMotor.get("drvBackLeft");
@@ -46,19 +70,19 @@ public class MichaelOp extends OpMode {
         drvIntakeRight = (DcMotorEx)hardwareMap.dcMotor.get("drvIntakeRight");
         drvLinearSlide = (DcMotorEx)hardwareMap.dcMotor.get("drvLinearSlide");
         drvVacuum = (DcMotorEx)hardwareMap.dcMotor.get("drvVacuum");
-
+        // Set motor run modes
         dtFrontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         dtFrontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         dtBackLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         dtBackRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         drvIntakeLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         drvIntakeRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
+        // Set drivetrain motor braking behavior (without this the robot will 'glide' after no power is given)
         dtFrontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         dtFrontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         dtBackLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         dtBackRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
+        // Instantiate boolean variables
         limiter = true;
         cTIntake = true;
         cTIntakeReverse = true;
@@ -68,43 +92,45 @@ public class MichaelOp extends OpMode {
         cTClaw = true;
         cTCorrector = true;
         cTLatch = true;
-
+        // Set motor directions
         dtFrontLeft.setDirection(DcMotor.Direction.REVERSE);
         dtBackLeft.setDirection(DcMotor.Direction.REVERSE);
         drvIntakeRight.setDirection(DcMotor.Direction.REVERSE);
         drvLinearSlide.setDirection(DcMotor.Direction.REVERSE);
-
+        // Instantiate servos
         srvRamp = hardwareMap.servo.get("srvRamp");
-        srvRamp.setPosition(1);
         srvPhone = hardwareMap.servo.get("srvPhone");
-        srvPhone.setPosition(0.5);
         srvRelicArm = hardwareMap.servo.get("srvRelicArm");
-        srvRelicArm.setPosition(RELIC_ARM_POSITION_DOWN);
         srvRelicClaw = hardwareMap.servo.get("srvRelicClaw");
-        srvRelicClaw.setPosition(RELIC_CLAW_POSITION_CLOSED);
         srvArmSwivel = hardwareMap.servo.get("srvArmSwivel");
-        srvArmSwivel.setPosition(0.5);
         srvArmShoulder = hardwareMap.servo.get("srvArmShoulder");
-        srvArmShoulder.setPosition(1);
         srvLatch = hardwareMap.servo.get("srvLatch");
+        // Set initial servo positions
+        srvRamp.setPosition(1);
+        srvPhone.setPosition(0.5);
+        srvRelicArm.setPosition(RELIC_ARM_POSITION_DOWN);
+        srvRelicClaw.setPosition(RELIC_CLAW_POSITION_CLOSED);
+        srvArmSwivel.setPosition(0.5);
+        srvArmShoulder.setPosition(1);
         srvLatch.setPosition(0);
-
+        // Set drive train to go forward
         driveAngle = Math.PI/4;
-        /*
-        DcMotorEx[] drivetrain = new DcMotorEx[] { dtFrontLeft, dtFrontRight, dtBackLeft, dtBackRight };
-
-        for (DcMotorEx motor : drivetrain) {
-            //PIDCoefficients cfs = motor.getPIDCoefficients(DcMotor.RunMode.RUN_TO_POSITION);
-            motor.setPIDCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, new PIDCoefficients(10,0,0));
-        }
-        */
-
         telemetry.addData("Status", "Hardware initialized");
     }
 
+
+    /**
+     * Reset runtime
+     * Executes upon start of TeleOp
+     */
     @Override
     public void start() { runtime.reset(); }
 
+
+    /**
+     * Translate driver controller inputs into onboard robot hardware commands
+     * Continuosly loops until completion of TeleOp
+     */
     @Override
     public void loop() {
         victorianDrive();
@@ -119,6 +145,11 @@ public class MichaelOp extends OpMode {
         sendTelemetry();
     }
 
+
+    /**
+     * Kill all drivetrain motors
+     * Executes upon completion of TeleOp
+     */
     @Override
     public void stop() {
         dtFrontLeft.setPower(0);
@@ -127,16 +158,19 @@ public class MichaelOp extends OpMode {
         dtBackRight.setPower(0);
     }
 
-    // Analog Controls for setting correct drive train power
+
+    /**
+     * Calculate and set drive train motor power
+     * Ability for driver to modify drivetrain power limiter
+     */
     private void victorianDrive() {
         // Collect controller inputs
         double lsx = gamepad1.left_stick_x, lsy = gamepad1.left_stick_y;
         double rsx = gamepad1.right_stick_x;
-        double epsilon = Math.sqrt(2);
         double r = Math.hypot(lsx, lsy);
         double stickAngle = Math.atan2(lsy, -lsx);
         // Set "front" of the robot
-        if(DPadPressed())
+        if(padPressed())
             setDriveAngle();
         // Calculate power angle derived from analog stick angle
         double powerAngle = stickAngle - driveAngle;
@@ -146,8 +180,8 @@ public class MichaelOp extends OpMode {
         double cos = r*Math.cos(powerAngle);
         double sin = r*Math.sin(powerAngle);
         // Ramp up power values
-        double cosPow = epsilon*cos;
-        double sinPow = epsilon*sin;
+        double cosPow = EPSILON*cos;
+        double sinPow = EPSILON*sin;
         // Calculate individual motor power values
         LFP = Range.clip(cosPow-turn, -1.0, 1.0);
         LRP = Range.clip(sinPow-turn, -1.0, 1.0);
@@ -158,39 +192,64 @@ public class MichaelOp extends OpMode {
         LRP*=DRIVE_MOD;
         RFP*=DRIVE_MOD;
         RRP*=DRIVE_MOD;
-
+        // Set drivetrain motor power
         dtFrontLeft.setPower(LFP);
         dtFrontRight.setPower(RFP);
         dtBackLeft.setPower(LRP);
         dtBackRight.setPower(RRP);
     }
-    //  Sets the vacuum motor
+
+
+    /**
+     * Set jewel collection motor power
+     */
     private void setVacuumPower() {
         drvVacuum.setPower(gamepad2.right_trigger - gamepad2.left_trigger);
     }
-    // Returns proper turn power using dual zone
+
+
+    /**
+     * Calculate turn power using "dual zone" piece-wise math
+     *
+     * @param t right stick x position
+     * @return drivetrain turn power
+     */
     private double getDualZonePower(double t) {
         if(Math.abs(t) < .5)
             return (.8*t*DRIVE_CAP);
         else
             return Math.signum(t)*(1.2*Math.abs(t)-0.2)*DRIVE_CAP;
     }
-    // Returns Boolean (determine if D-Pad is being pressed)
-    private boolean DPadPressed() {
+
+
+    /**
+     * Determine if D-Pad is being pressed
+     *
+     * @return dpad being pressed
+     */
+    private boolean padPressed() {
         return (gamepad1.dpad_up || gamepad1.dpad_down || gamepad1.dpad_left || gamepad1.dpad_right);
     }
-    // Sets the "front" of the robot
+
+
+    /**
+     * Set the "front" of the robot
+     */
     private void setDriveAngle() {
         if(gamepad1.dpad_up)
             driveAngle = Math.PI/4;
         else if(gamepad1.dpad_left)
-            driveAngle = 3*(Math.PI/4);
+            driveAngle = -(Math.PI/4);
         else if(gamepad1.dpad_down)
             driveAngle = -3*(Math.PI/4);
         else if(gamepad1.dpad_right)
-            driveAngle = -(Math.PI/4);
+            driveAngle = 3*(Math.PI/4);
     }
-    // Toggles Intake Motor Power
+
+
+    /**
+     * Toggle intake motor power
+     */
     private void setIntakePower() {
         boolean x = (gamepad1.x || gamepad2.x), y = (gamepad1.y || gamepad2.y);
         boolean c = (gamepad1.left_trigger > 0.1 || gamepad1.right_trigger > 0.1);
@@ -243,7 +302,11 @@ public class MichaelOp extends OpMode {
         drvIntakeLeft.setPower(intakePower);
         drvIntakeRight.setPower(intakePower);
     }
-    // Toggles ramp position
+
+
+    /**
+     * Toggles ramp position
+     */
     private void setRampPosition() {
         boolean lb = gamepad1.left_bumper, rb = gamepad1.right_bumper;
         // Both bumpers raise the ramp
@@ -255,11 +318,19 @@ public class MichaelOp extends OpMode {
         else if(!lb && !rb)
             cTRamp = true;
     }
-    // Controls Linear Slide Power
+
+
+    /**
+     * Set linear slide motor power
+     */
     private void setLinearSlide() {
         drvLinearSlide.setPower(gamepad2.left_stick_y);
     }
-    // Toggles Relic Arm & Relic Claw Positions
+
+
+    /**
+     * Toggle relic arm & relic claw servo positions
+     */
     private void setLinearServos() {
         boolean lb = gamepad2.left_bumper, rb = gamepad2.right_bumper;
         // Toggles Relic Arm Position
@@ -289,25 +360,25 @@ public class MichaelOp extends OpMode {
         } else if (!a)
             cTLatch = true;
     }
-    // Controls Linear Slide Power
+
+
+    /**
+     * Set jewel arm servo positions
+     */
     private void setJewelArm() {
-        /*double xAngle = srvArmSwivel.getPosition() + (0.1 * gamepad2.right_stick_x);
-        double yAngle = srvArmShoulder.getPosition() + (0.1 * gamepad2.right_stick_y);
-
-        srvArmSwivel.setPosition((gamepad2.right_stick_x + 1) / 2);
-        srvArmShoulder.setPosition((gamepad2.right_stick_y + 1) / 2);*/
-
-        final double speed = 0.001;
+        final double speed = 0.002;
         final double deadzone = 0.05;
         final double xSpeed = (Math.abs(gamepad2.right_stick_x) < deadzone ? 0 : speed * Math.signum(gamepad2.right_stick_x));
         final double ySpeed = (Math.abs(gamepad2.right_stick_y) < deadzone ? 0 : speed * Math.signum(gamepad2.right_stick_y));
 
         srvArmSwivel.setPosition(srvArmSwivel.getPosition() + xSpeed);
         srvArmShoulder.setPosition(srvArmShoulder.getPosition() + ySpeed);
-
-
     }
-    // Sets thee motor restrictions of the robot's motors
+
+
+    /**
+     * Set intake and drivetrain motor power limiter
+     */
     private void setLimiter() {
         boolean ls = gamepad1.left_stick_button, rs = gamepad1.right_stick_button;
         // Both stick buttons toggle intake mods
@@ -322,7 +393,11 @@ public class MichaelOp extends OpMode {
         else if(!ls && !rs)
             cTLimiter = true;
     }
-    // Run concurrent isntructions from the schedule
+
+
+    /**
+     * Run concurrent isntructions from the schedule
+     */
     private void runConcurrents() {
         for (int i = schedule.size() - 1; i >= 0; i--) {
             ConcurrentInstruction inst = schedule.get(i);
@@ -334,7 +409,11 @@ public class MichaelOp extends OpMode {
             } catch (AbortException e) {}
         }
     }
-    // Send Telemetry
+
+
+    /**
+     * Send onboard robot telemetry to driver station
+     */
     private void sendTelemetry() {
         telemetry.addData("Status", "Run Time: " + runtime.toString());
         telemetry.addData("Front Motors", "Left (%.2f) | Right (%.2f)", LFP, RFP);
@@ -344,7 +423,11 @@ public class MichaelOp extends OpMode {
         telemetry.addData("Ramp Engaged",ramp);
         telemetry.update();
     }
-    // Schedule an instruction to be executed some time from now
+
+
+    /**
+     * Schedule an instruction to be executed some time from now
+     */
     private void scheduleConcurrent(NestedInstruction nested, double inTime) {
         schedule.add(new ConcurrentInstruction(nested, runtime.time() + inTime));
     }
